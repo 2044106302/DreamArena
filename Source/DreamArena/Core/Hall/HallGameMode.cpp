8ffe2ce5ed77ed05b165435e6a2ebcd792b8d1ef
FullX <104566348+2044106302@users.noreply.github.com>
2024-04-08 23:05:12 +0800

    temp
    增添基础的GamePlay框架

---
+++ Source/DreamArena/Core/Hall/HallGameMode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HallGameMode.h"
+#include "HallPlayerController.h"
+#include "HallHUD.h"
+
+AHallGameMode::AHallGameMode()
+{
+	HUDClass = AHallHUD::StaticClass();
+	PlayerControllerClass = AHallPlayerController::StaticClass();
+	DefaultPawnClass = nullptr;
+
+}
